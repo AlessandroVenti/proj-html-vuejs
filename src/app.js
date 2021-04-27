ac63f7c0429c405e7ce90786c90a75c8c1f77b40
Alessandro Venti <alessandro.venti@icloud.com>
2021-04-27 13:15:13 +0200

    Working on header with vue

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 function createVueInstance() {
      new Vue({
+          el: '#vueSofbox',
 
+          data: {
+
+               logoImage: 'logo-2.png',
+               navCenterList: ['Home','About us','Feature','Testimonials','Contact US'],
+               navIconsList: ['fas fa-search','fas fa-shopping-cart']
+          }
      });
    }
    
