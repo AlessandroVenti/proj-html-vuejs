f0e45dfd2f80b1daf43c5cc48c0b6dfa60fa2192
Alessandro Venti <alessandro.venti@icloud.com>
2021-04-27 15:42:17 +0200

    Navbar(header) done in flex and vue with animations

--- src/app.js
+++ src/app.js
@@ -5,8 +5,46 @@ function createVueInstance() {
           data: {
 
                logoImage: 'logo-2.png',
+               // ITEMS LISTED IN NAV
                navCenterList: ['Home','About us','Feature','Testimonials','Contact US'],
-               navIconsList: ['fas fa-search','fas fa-shopping-cart']
+               // FLAG FOR INPUT IN NAV
+               inputNavFlag: false,
+               // FLAG FOR ITEMS LISTED IN NAV
+               inputNavItem: false,
+               // COUNTER FOR SHOPPING CART IN NAVBAR
+               shoppingCounter: 0
+          },
+
+          methods: {
+
+               // GENERATE EFFECT OF THE SEARCHBAR IN NAV
+
+               navEffectInput: function() {
+                    if(this.inputNavFlag == false) {
+                         this.inputNavFlag = true;
+                    } else{
+                         this.inputNavFlag = false;
+                    }
+                    console.log(this.inputNavFlag);
+               },
+
+                    // GENERATE EFFECT OF THE ITEMS LISTED IN NAV
+
+               navEffectItem: function() {
+                    if(this.inputNavItem == false) {
+                         this.inputNavItem = true;
+                    } else{
+                         this.inputNavItem = false;
+                    }
+                    console.log(this.inputNavItem);
+               },
+
+                         // INCREASE THE SHOPPING COUNTER IN NAV
+
+               increaseShoppingCart: function() {
+                    this.shoppingCounter++;
+               }
+
           }
      });
    }
